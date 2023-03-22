a20b083e721eb4326de06f3871c3814028743dcd
Elbadil <elbaliadil@gmail.com>
2023-03-21 17:19:57 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - to add numbers
+ * @n: number 1
+ * @l : number 2
+ * Return: Always 0;
+ */
+int add(int n, int l)
+{
+	int sum;
+
+	sum = n + l;
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ *
+ * times_table - table 9 times
+ * Return: always 0.
+ */
+
+
