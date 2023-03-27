fce9994b8be6e246638bf755edd7001121ac9037
Elbadil <elbaliadil@gmail.com>
2023-03-27 14:42:33 -0700

    String Printed

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * _strlen - To calculate the lenght of a string
- * @s: Our parameter
+ * @s: Our tring parameter
  * Return: Length of the string
  */
 int _strlen(char *s)
 {
-	int l = 0;
+	int l;
 
-	for (; *s++;)
+	for (l = 0 ; s[l] != '\0';)
 		l++;
 	return (l);
 }
--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,9 +7,9 @@
  */
 void _puts(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (str[i] != '\0')
+	for (i = 0 ; str[i] != '\0' ;)
 	{
 		_putchar(str[i]);
 		i++;
