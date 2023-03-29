f90a8373fb3eda7527bdd18311a4509ae0977980
Elbadil <elbaliadil@gmail.com>
2023-03-29 09:54:58 -0700

    concatenate two string and print depending on bytes

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncat - To concatenates two strings
+ * @dest: First string
+ * @src : Second string
+ * @n: bytes
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int l;
+	int i;
+
+	for (l = 0 ; dest[l] != '\0' ;)
+		l++;
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[l] = src[i];
+		i++;
+		l++;
+	}
+	dest[l] = '\0';
+	return (dest);
+}
