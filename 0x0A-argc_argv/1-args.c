b23dc7723b9903fd4428ab1cb35ad679dc4147c6
Elbadil <elbaliadil@gmail.com>
2023-04-08 08:08:35 -0700

    Number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: array of character pointers that list all arguments
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
