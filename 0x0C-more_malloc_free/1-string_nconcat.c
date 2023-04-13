3dbeea55f3409ae25cfb847021f9244d86065333
Elbadil <elbaliadil@gmail.com>
2023-04-13 10:42:42 -0700

    Concatenate two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,32 +10,37 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
-	unsigned int i, j;
+	unsigned int i = 0, j = 0, l1 = 0, l2 = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	i = 0;
-	while (s1[i] != '\0')
-		i++;
+	while (s1[l1] != '\0')
+		l1++;
+	while (s2[l2] != '\0')
+		l2++;
 
-	j = 0;
-	while (s2[j] != '\0')
-		j++;
+	if (n < l2)
+		s = malloc(sizeof(char) * (l1 + n + 1));
+	else
 
-	s = malloc(sizeof(char) * (i + j + 1));
+		s = malloc(sizeof(char) * (l1 + l2 + 1));
 	if (s == NULL)
 		return (NULL);
 
-	i = 0;
 	while (s1[i] != '\0')
 	{
 		s[i] = s1[i];
 		i++;
 	}
-	j = 0;
+	while (n < l2 && i < (l1 + n))
+	{
+		s[i] = s2[j];
+		j++;
+		i++;
+	}
 	while (j <= n && s2[j] != '\0')
 	{
 		s[i] = s2[j];
