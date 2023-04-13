3ea17debb5102f9ea4243baddfd3d41dafebe469
Elbadil <elbaliadil@gmail.com>
2023-04-13 11:17:40 -0700

    Concatenate two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,31 +10,31 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	unsigned int i = 0, j = 0, l1 = 0, l2 = 0;
 
-	while (s1 != NULL && s1[len1] != '\0')
-		len1++;
-	while (s2 != NULL && s2[len2] != '\0')
-		len2++;
+	while (s1 != NULL && s1[l1] != '\0')
+		l1++;
+	while (s2 != NULL && s2[l2] != '\0')
+		l2++;
 
-	if (n < len2)
-		s = malloc(sizeof(char) * (len1 + n + 1));
+	if (n < l2)
+		s = malloc(sizeof(char) * (l1 + n + 1));
 	else
-		s = malloc(sizeof(char) * (len1 + len2 + 1));
+		s = malloc(sizeof(char) * (l1 + l2 + 1));
 
 	if (s == NULL)
 		return (NULL);
 
-	while (i < len1)
+	while (i < l1)
 	{
 		s[i] = s1[i];
 		i++;
 	}
 
-	while (n < len2 && i < (len1 + n))
+	while (n < l2 && i < (l1 + n))
 		s[i++] = s2[j++];
 
-	while (n >= len2 && i < (len1 + len2))
+	while (n >= l2 && i < (l1 + l2))
 		s[i++] = s2[j++];
 
 	s[i] = '\0';
