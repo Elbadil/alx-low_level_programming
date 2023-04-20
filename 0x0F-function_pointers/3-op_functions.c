6344937a428c2bacd8379f36eaa8226f646cdbbf
Elbadil <elbaliadil@gmail.com>
2023-04-19 17:19:28 -0700

    Operator Function Pointer

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * op_add - adds two integers
+ * @a: first int
+ * @b: second int
+ * Return: a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subs two integers
+ * @a: first int
+ * @b: second int
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies two integers
+ * @a: first int
+ * @b: second int
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divs two integers
+ * @a: first int
+ * @b: second int
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - mods two integers
+ * @a: first int
+ * @b: second int
+ * Return: a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
