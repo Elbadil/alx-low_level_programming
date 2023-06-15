6940f97d02b207a0aa21634b73235c352fd1e764
Elbadil <elbaliadil@gmail.com>
2023-06-15 10:13:09 -0700

    Add node at a specific index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *temp = *h;
 	unsigned int i = 0;
 
-	if (*h == NULL)
+	if (*h == NULL || h == NULL)
 		return (NULL);
 
 	newn = malloc(sizeof(dlistint_t));
