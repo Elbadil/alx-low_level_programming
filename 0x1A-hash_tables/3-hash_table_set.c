a4b0a88804f17958a126b7ebb73cb94c45062ca6
Elbadil <elbaliadil@gmail.com>
2023-07-07 07:14:36 -0700

    Add elemnt to hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,32 +11,41 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_elmnt;
-	unsigned long int index;
+	unsigned long int index, j;
+	char *value_cp, *key_cp;
 
 	if (ht == NULL || key == NULL || value == NULL || strlen(key) == 0)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
 
+	value_cp = strdup(value);
+	if (value_cp == NULL)
+		return (0);
+
+	for (j = index; ht->array[j] != NULL; j++)
+	{
+		if (strcmp(ht->array[j]->key, key) == 0)
+		{
+			free(ht->array[j]->value);
+			ht->array[j]->value = value_cp;
+			return (1);
+		}
+	}
+
 	new_elmnt = malloc(sizeof(hash_node_t));
 	if (new_elmnt == NULL)
 		return (0);
 
-	new_elmnt->key = strdup(key);
-	if (new_elmnt->key == NULL)
+	key_cp = strdup(key);
+	if (key_cp == NULL)
 	{
 		free(new_elmnt);
 		return (0);
 	}
 
-	new_elmnt->value = strdup(value);
-	if (new_elmnt->value == NULL)
-	{
-		free(new_elmnt->key);
-		free(new_elmnt);
-		return (0);
-	}
-
+	new_elmnt->key = key_cp;
+	new_elmnt->value = value_cp;
 	new_elmnt->next = ht->array[index];
 	ht->array[index] = new_elmnt;
 
