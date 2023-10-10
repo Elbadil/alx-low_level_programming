26d158839770a5596a16e41ad174f9580cac3950
Elbadil <elbaliadil@gmail.com>
2023-10-10 18:49:09 +0100

    Jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -16,7 +16,7 @@ int jump_search(int *array, size_t size, int value)
 	size_t left;
 	size_t right;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
 
 	while (array[i] < value && i < size)
